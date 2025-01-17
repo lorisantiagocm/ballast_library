c9f1c212a23a5123225587098851cc5616111381
Laura Santiago Campos Oliveira <laaaaaaa45@gmail.com>
2025-01-17 08:40:24 -0300

    💄 Create admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,7 +1,11 @@
 module Admin
   class DashboardController < ApplicationController
     def index
-      # @books = Book.all
+      @book_amount = Book.all.count
+      @book_copies_amount = Book.all.sum(:total_copies)
+      @current_borrows_amount = Borrow.all.current.count
+      @borrows_due_today = Borrow.all.due_today.count
+      @overdue_borrows = Borrow.all.overdue
     end
   end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   include Pundit::Authorization
 
-  before_action :restrict_admin_access, if: -> { user_signed_in? && current_user.member? }
+  before_action :restrict_admin_access, if: -> { user_signed_in? }
 
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
